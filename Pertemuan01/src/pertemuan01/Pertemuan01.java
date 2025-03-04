ce8d08406e863907f6e1f2fd53c1f22ccb42bb8f
PC-F-35 <aurora.1412.4869@gmail.com>
2025-03-04 21:38:44 +0800

    Pertemuan 1

---
+++ Pertemuan01/src/pertemuan01/Pertemuan01.java
@@ -0,0 +1,16 @@
+package pertemuan01;
+
+/**
+ *
+ * @author Gabriel Friskila
+ */
+public class Pertemuan01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Belajar Bahasa Java");
+    }
+    
+}
